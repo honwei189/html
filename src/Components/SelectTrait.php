f7da6b7f15fd2fd3f1c77bf13496d12d63f43831
Gordon Lim <honwei189@gmail.com>
2022-07-09 20:18:17 +0800

    Added new function -- time_menu() and select_time()

--- src/Components/SelectTrait.php
+++ src/Components/SelectTrait.php
@@ -227,4 +227,109 @@ trait SelectTrait
 
         return $this->set_object_type(__METHOD__)->output_as($this->select($name, $value, $attrs, $options_data, $optional_option));
     }
+
+    /**
+     * Alias function of the time_menu()
+     *
+     * @param string $name The name of the select menu
+     * @param int $minutes_steps This is the number of minutes to increment by. For example, if you want to
+     * increment by 15 minutes, you would set this to 15.
+     * @param bool $with_seconds true/false.  Display "seconds" drop down menu or not.
+     * @param string $default_value The default value of the time input.
+     * @param array $optional_option If you want to have a blank option at the top of the select box, set this
+     * to true.
+     *
+     * @return ?string Return time menu or directly print out the select menu
+     */
+    public function select_time($name, $minutes_steps = 0, $with_seconds = false, $default_value = "", $optional_option = false): ?string
+    {
+        return $this->time_menu($name, $minutes_steps, $with_seconds, $default_value, $optional_option);
+    }
+
+    /**
+     * It creates a drop down select based time menu.
+     *
+     * @param string $name The name of the select element
+     * @param int $minutes_steps This is the number of minutes to increment by. For example, if you want to
+     * increment by 15 minutes, you would set this to 15.
+     * @param bool $with_seconds true/false.  Display "seconds" drop down menu or not.
+     * @param string $default_value The default value of the time input.
+     * @param array $optional_option If you want to have a blank option at the top of the select box, set this
+     * to true.
+     *
+     * @return ?string Return time menu or directly print out the select menu
+     */
+    public function time_menu($name, $minutes_steps = 0, $with_seconds = false, $default_value = "", $optional_option = false): ?string
+    {
+        if ($this->object != "html::select_time_input_group") {
+            $this->object = __METHOD__;
+        }
+
+        $hr   = ($optional_option ? ["" => "Hour"] : []);
+        $min  = ($optional_option ? ["" => "Minutes"] : []);
+        $sec  = [];
+        $zone = ["am" => "AM", "pm" => "PM"];
+
+        if (is_value($default_value)) {
+            list($hr_v, $min_v, $sec_v) = explode(":", $default_value);
+
+            if ($hr_v == 0) {
+                $hr_v   = 12;
+                $zone_v = "am";
+            } else if ($hr_v > 12) {
+                $hr_v   = $hr_v;
+                $zone_v = "pm";
+            } else if ($hr_v > 12) {
+                $hr_v   = $hr_v - 12;
+                $zone_v = "pm";
+            } else {
+                $hr_v   = $hr_v;
+                $zone_v = "am";
+            }
+        }
+
+        if ($optional_option) {
+            $zone = array_merge(["" => "AM / PM"], $zone);
+        }
+
+        for ($i = 1; $i <= 12; ++$i) {
+            // $hr .= "\t<option value=\"$i\">$i</option>" . PHP_EOL;
+            $hr[$i] = sprintf("%02d", $i);
+        }
+
+        $minutes_steps = (int) $minutes_steps;
+
+        if ($minutes_steps > 0) {
+            $steps = ceil(60 / (int) $minutes_steps);
+        } else {
+            $steps = 60;
+        }
+
+        for ($i = 0; $i < $steps; ++$i) {
+            $m       = $i * $minutes_steps;
+            $min[$m] = sprintf("%02d", $m);
+        }
+
+        unset($m);
+
+        $sec = array_map(function ($data) {
+            return sprintf("%02d", $data);
+        }, range(0, 59));
+
+        $sec   = array_merge(["" => "Seconds"], $sec);
+        $attr  = $this->attr;
+        $class = $this->class;
+
+        $hr   = $this->set_object_type(__METHOD__)->data($hr)->select($name . "_hr", $hr_v, $optional_option);
+        $min  = $this->set_object_type(__METHOD__)->attr($attr)->class($class)->data($min)->select($name . "_min", $min_v, $optional_option);
+        $zone = $this->set_object_type(__METHOD__)->attr($attr)->class($class)->data($zone)->select($name . "_zone", $zone_v, $optional_option);
+
+        if ($with_seconds) {
+            $sec = " " . $this->set_object_type(__METHOD__)->attr($attr)->class($class)->data($sec)->select($name . "_sec", $sec_v, $optional_option);
+        } else {
+            $sec = "";
+        }
+
+        return $this->output_as($hr . " " . $min . $sec . " " . $zone);
+    }
 }
